4648266958f838bd6c134b60a0999083759dabcc
Knoxx04 <onyiknoxx3@gmail.com>
2023-01-11 09:51:19 +0300

    completed optional

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>About Me</h2>
+  <p><p>Lorem ipsum dolor sit amet. Eos omnis autem et aperiam nulla qui iste natus. 33 impedit placeat quo unde earum et error pariatur sit vero dolorum aut praesentium rerum in omnis incidunt et soluta temporibus. </p><p>Et accusamus pariatur nam voluptas nihil et autem voluptates aut obcaecati nemo ut dolorem iure est aperiam ipsum? Aut omnis nemo qui itaque amet et quos architecto sit dolorem aperiam ut omnis quibusdam. </p><p>Ut minima dignissimos et suscipit nihil est dolores molestias rem iure quia aut deleniti veritatis et inventore consectetur. Et dolor nemo in numquam molestiae rem saepe enim ut voluptatem omnis non delectus commodi est quod similique. Aut quam tempora sed ipsa similique in expedita aspernatur a delectus laudantium sit debitis atque. Qui magni sunt et nesciunt tempora At aperiam incidunt aut animi temporibus ut earum pariatur ut voluptate culpa ea neque galisum. </p>
+  </p>
+<img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
